45807b77f09217bcffa8c8c1046b426fe1921cd0
Bethashcroft <bethashcroft1998@gmail.com>
2022-12-06 15:36:27 +0000

    ticket 5 done

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-const ArticleCard = ({ article }) => {
+const SingleArticleCard = ({ article }) => {
   return (
     <ul>
       <h2>{article.title}</h2>
       <h3>{article.article_id}</h3>
       <h4>Written by {article.author}</h4>
       <h4>Votes: {article.votes}</h4>
+      <p>{article.body}</p>
     </ul>
   );
 };
 
-export default ArticleCard;
+export default SingleArticleCard;
